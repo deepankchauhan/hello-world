fe6236940a1d077839635db222814e50d1b28aef
Deepank Chauhan <31995183+deepankchauhan@users.noreply.github.com>
2019-01-12 23:24:22 +0530

    Say hello in javascript

--- Hello.js
+++ Hello.js
@@ -1,6 +1,6 @@
-//A hello world program in javascript
+// A hello world program in javascript
 
 // this code works on command line, first enter "node" (then enter) in the command and then this code.
 
-//this prints the output
+// this prints the output
 console.log('Hello, World!\n');
