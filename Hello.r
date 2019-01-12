e11a5565eba0981f3ee97208d28f8193fbea2df9
Deepank Chauhan <31995183+deepankchauhan@users.noreply.github.com>
2019-01-12 23:40:10 +0530

    Say hello in R

---
+++ Hello.r
@@ -0,0 +1,10 @@
+# A hello world program in R
+
+# Install R using the command $ sudo apt-get install r-cran-littler
+
+# Once installed, enter "r" in the command line (then enter) continued with this code below.
+
+myString <- "Hello, World!"
+
+# this prints the output
+print(myString)
