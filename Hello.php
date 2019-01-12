0e9ae1763bc81e05cd6ae9be3aa65c8f73d895c2
Deepank Chauhan <31995183+deepankchauhan@users.noreply.github.com>
2019-01-12 23:25:30 +0530

    Say hello in php

--- Hello.php
+++ Hello.php
@@ -1,5 +1,6 @@
 <!-- A hello world program in php -->
 <!-- Run this code on the command line using the command "php (filename)" -->
+
 <?php
     # this prints the output
     echo 'Hello, World!\n';
