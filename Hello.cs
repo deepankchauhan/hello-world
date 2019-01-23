9127af910e944e62c1037980256e389950c626b2
Deepank Chauhan <deepankchauhan73@gmail.com>
2019-01-23 23:55:15 +0530

    Say hello in C#

--- Hello.cs
+++ Hello.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+                // this prints the output
                 Console.WriteLine("Hello, world!");
                 Console.ReadLine();
         }
